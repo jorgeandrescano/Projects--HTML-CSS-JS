cf97f1749d2fe26bd147c7859597a43f086084fd
jorgeandrescano <61289656+jorgeandrescano@users.noreply.github.com>
2023-08-22 17:38:30 -0500

    added javascript functionality

---
+++ projects/calculator-v2/main.js
@@ -0,0 +1,88 @@
+const numberBtn = document.querySelectorAll("[data-number]");
+const operatorBtn = document.querySelectorAll("[data-operator]");
+const equalBtn = document.querySelector("[data-equal]");
+const resetBtn = document.querySelector("[data-reset]");
+const clearElBtn = document.querySelector("[data-clearEl]");
+const topValueText = document.querySelector("[data-topValue]");
+const botValueText = document.querySelector("[data-bottomValue]");
+
+class Calculator {
+  constructor(botValueText, topValueText) {
+    this.botValueText = botValueText;
+    this.topValueText = topValueText;
+    this.botValue = "";
+    this.topValue = "";
+    this.operator = undefined;
+  }
+
+  addNumber(number) {
+    this.botValue = this.botValue + number;
+    //this.topValue = 0
+  }
+  printDisplay() {
+    this.botValueText.innerText = this.botValue;
+    this.topValueText.innerText = this.topValue;
+  }
+  clearEl() {
+    this.botValue = this.botValue.slice(0, -1);
+  }
+  selectOperation(operator) {
+    if (this.botValue == "") return;
+    if (this.topValue != "") {
+      this.makeCalculation();
+    }
+    this.operator = operator;
+    this.topValue = this.botValue;
+    this.botValue = "";
+  }
+  makeCalculation() {
+    let result;
+    let convertTopValue = parseFloat(this.topValue);
+    let convertBotValue = parseFloat(this.botValue);
+
+    switch (this.operator) {
+      case "+":
+        result = convertTopValue + convertBotValue;
+        break;
+      case "-":
+        result = convertTopValue - convertBotValue;
+        break;
+      case "&times":
+        result = convertTopValue * convertBotValue;
+        break;
+      case "&divide":
+        result = convertTopValue / convertBotValue;
+        convertBotValue != 0 ? result : "Error Zero Division";
+        break;
+      case "%":
+        result = convertTopValue * (convertBotValue / 100);
+        break;
+      default:
+        return;
+    }
+    this.botValue = result
+    this.operator = undefined
+    this.topValue = ''
+  }
+}
+
+const calculator = new Calculator(botValueText, topValueText);
+
+numberBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    calculator.addNumber(btn.innerText);
+    calculator.printDisplay();
+  });
+});
+
+clearElBtn.addEventListener("click", () => {
+  calculator.clearEl();
+  calculator.printDisplay();
+});
+
+operatorBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    calculator.selectOperation(btn.innerText);
+    calculator.printDisplay();
+  });
+});
